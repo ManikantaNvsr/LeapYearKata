79b1ce1e9547305360eaa6496ec295e21a5468ba
ManikantaNvsr <50462892+ManikantaNvsr@users.noreply.github.com>
2020-05-20 09:45:27 +0530

    Second Testcase is added

--- src/main/java/org/leapyearkata/LeapYear.java
+++ src/main/java/org/leapyearkata/LeapYear.java
@@ -10,4 +10,8 @@ public class LeapYear {
             return false;
         }
     }
+
+    public boolean isItDivisibleByHundred(int i) {
+        return false;
+    }
 }
--- src/test/java/org/leapyearkata/LeapYearTests.java
+++ src/test/java/org/leapyearkata/LeapYearTests.java
@@ -13,4 +13,12 @@ public class LeapYearTests {
         boolean flag = leapYear.isItDivisibleByFourHundred(1200);
         assertTrue(flag);
     }
+
+    // test case for checking whether the given input year is divisible by 100
+    @Test
+    public void checkDivisibilityByHundred() {
+        LeapYear leapYear = new LeapYear();
+        boolean flag = leapYear.isItDivisibleByHundred(1200);
+        assertTrue(flag);
+    }
 }
