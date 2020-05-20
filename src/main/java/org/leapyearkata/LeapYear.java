cdd12ffc1089a7d2fe2bdc504809fdb6279c0133
ManikantaNvsr <50462892+ManikantaNvsr@users.noreply.github.com>
2020-05-20 09:55:11 +0530

    Prod Code related to Fourth Testcase is added.

--- src/main/java/org/leapyearkata/LeapYear.java
+++ src/main/java/org/leapyearkata/LeapYear.java
@@ -2,6 +2,11 @@ package org.leapyearkata;
 
 public class LeapYear {
 
+    // These are the import criteria which help us to decide whether a input year is leap year or not
+    static boolean divisibilityCheckByFourHundred = false;
+    static boolean divisibilityCheckByHundred = false;
+    static boolean divisibilityCheckByFour = false;
+
     // Finding whether the input year is divisible by 400 or not
     public static boolean isItDivisibleByFourHundred(int year) {
         if (year > 0 && year % 400 == 0) {
@@ -29,7 +34,20 @@ public class LeapYear {
         }
     }
 
-    public boolean isItLeapYear(int i) {
-        return false;
+    // Finding whether the input matches with the Leap Year Criteria or not
+    public static boolean isItLeapYear(int year) {
+        divisibilityCheckByFour = isItDivisibleByFour(year);
+        divisibilityCheckByFourHundred = isItDivisibleByFourHundred(year);
+        divisibilityCheckByHundred = isItDivisibleByHundred(year);
+        // If the given year is divisible by 400 then it's surely a Leap Year
+        if (divisibilityCheckByFourHundred) {
+            return true;
+            // If the given year is divisible by 4 and not divisible by 100 then it's a Leap Year
+        } else if (divisibilityCheckByFour && !divisibilityCheckByHundred) {
+            return true;
+            // Apart from the any other case than the mentioned comments above, it's not a Leap Year
+        } else {
+            return false;
+        }
     }
 }
